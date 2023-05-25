cd06c1c412f28f98feb5a870d0c4bf27b3e39576
Eleosse <AJ>
2023-05-25 18:48:36 +0200

    avancement section 5

--- src/scripts/app.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var messageContainer = document.querySelector('.bandeau-texte');
-    messageContainer.style.animation = 'defilement 30s linear infinite';
-  });
