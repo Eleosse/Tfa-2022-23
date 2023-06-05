d8748c7169a513249b5b97a89af8b9c49de8ac08
Eleosse <AJ>
2023-06-05 13:35:30 +0200

    debut anim

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,7 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 
 /* prof */
 var ptoggleProf = document.querySelector(".p__prof");
@@ -69,7 +72,7 @@ window.addEventListener('scroll', function() {
     }
   });
 
-
+ /* user btn */
 var cadreUser = document.querySelector(".legende__user");
 cadreUser.addEventListener("click", afficherCadre);
   
@@ -87,7 +90,7 @@ function masquerCadre() {
     document.getElementById('cadre-user').style.animation = 'fade-out 0.3s forwards';
 
   }
-
+/* renc btn */
 var cadreRenc = document.querySelector(".legende__renc");
 cadreRenc.addEventListener("click", afficherCadreR);
   
@@ -105,3 +108,98 @@ function masquerCadreR() {
     document.getElementById('cadre-renc').style.animation = 'fade-out 0.3s forwards';
 
   }
+
+
+var blurrHypo1 = document.querySelector('.hypo__para--1')
+var blurrHypo2 = document.querySelector('.hypo__para--2')
+var blurrHypo3 = document.querySelector('.hypo__para--3')
+/* hypo1 btn */
+var cadreH1 = document.querySelector(".hypo__link--1");
+cadreH1.addEventListener("click", afficherCadreH1);
+  
+function afficherCadreH1() {
+    document.getElementById('cadre-h1').style.display = 'block';
+    document.getElementById('cadre-h1').style.animation = 'fade-in 0.3s forwards';
+    blurrHypo1.classList.add('blurr__hypo');
+    blurrHypo2.classList.add('blurr__hypo');
+    blurrHypo3.classList.add('blurr__hypo');
+}
+
+var croixH1 =  document.getElementById('croix-h1');
+croixH1.addEventListener('click', masquerCadreH1);
+
+function masquerCadreH1() {
+    document.getElementById('cadre-h1').style.display = 'none';
+    document.getElementById('cadre-h1').style.animation = 'fade-out 0.3s forwards';
+    blurrHypo1.classList.remove('blurr__hypo');
+    blurrHypo2.classList.remove('blurr__hypo');
+    blurrHypo3.classList.remove('blurr__hypo');
+  }
+
+/* hypo2 btn */
+var cadreH2 = document.querySelector(".hypo__link--2");
+cadreH2.addEventListener("click", afficherCadreH2);
+  
+function afficherCadreH2() {
+    document.getElementById('cadre-h2').style.display = 'block';
+    document.getElementById('cadre-h2').style.animation = 'fade-in 0.3s forwards';
+    blurrHypo1.classList.add('blurr__hypo');
+    blurrHypo2.classList.add('blurr__hypo');
+    blurrHypo3.classList.add('blurr__hypo');
+}
+
+var croixH2 =  document.getElementById('croix-h2');
+croixH2.addEventListener('click', masquerCadreH2);
+
+function masquerCadreH2() {
+    document.getElementById('cadre-h2').style.display = 'none';
+    document.getElementById('cadre-h2').style.animation = 'fade-out 0.3s forwards';
+    blurrHypo1.classList.remove('blurr__hypo');
+    blurrHypo2.classList.remove('blurr__hypo');
+    blurrHypo3.classList.remove('blurr__hypo');
+  }
+
+/* hypo3 btn */
+var cadreH3 = document.querySelector(".hypo__link--3");
+cadreH3.addEventListener("click", afficherCadreH3);
+  
+function afficherCadreH3() {
+    document.getElementById('cadre-h3').style.display = 'block';
+    document.getElementById('cadre-h3').style.animation = 'fade-in 0.3s forwards';
+    blurrHypo1.classList.add('blurr__hypo');
+    blurrHypo2.classList.add('blurr__hypo');
+    blurrHypo3.classList.add('blurr__hypo');
+}
+
+var croixH3 =  document.getElementById('croix-h3');
+croixH3.addEventListener('click', masquerCadreH3);
+
+function masquerCadreH3() {
+    document.getElementById('cadre-h3').style.display = 'none';
+    document.getElementById('cadre-h3').style.animation = 'fade-out 0.3s forwards';
+    blurrHypo1.classList.remove('blurr__hypo');
+    blurrHypo2.classList.remove('blurr__hypo');
+    blurrHypo3.classList.remove('blurr__hypo');
+  }
+
+const paragraphs = document.querySelectorAll('.p--anim');
+
+paragraphs.forEach((element) => {
+    gsap.fromTo(element, {
+        opacity: 0,
+        y: 25
+    }, {
+        opacity: 1,
+        y: 0,
+        duration: 2,
+        ease: 'power2.out',
+        scrollTrigger: {
+            trigger: element,
+            start: 'top 80%', 
+            end: 'bottom 20%', 
+            toggleClass: 'active', 
+            
+        }
+    });
+});
+
