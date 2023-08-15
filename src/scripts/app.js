d109a59766f208c3bc3cc071db3b5e462a709208
Eleosse <AJ>
2023-08-15 18:03:32 +0200

    suppr cadre + new page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,60 +3,6 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-/* prof */
-var ptoggleProf = document.querySelector(".p__prof");
-var iconeUser1 =  document.querySelector(".icone__user1");
-if(iconeUser1){
-    iconeUser1.addEventListener("click",text_add1);
-}
-function text_add1(){
-    ptoggleProf.classList.add('p__toggle');
-    iconeUser1.classList.add('blurr__icone');
-}
-if(ptoggleProf){
-    ptoggleProf.addEventListener("click",text_supr1);
-}
-function text_supr1(){
-    ptoggleProf.classList.remove('p__toggle');
-    iconeUser1.classList.remove('blurr__icone');
-}
-
-/* joue */
-var ptoggleJoue = document.querySelector(".p__joue");
-var iconeUser2 =  document.querySelector(".icone__user2");
-if(iconeUser2){
-    iconeUser2.addEventListener("click",text_add2);
-}
-function text_add2(){
-    ptoggleJoue.classList.add('p__toggle');
-    iconeUser2.classList.add('blurr__icone');
-}
-if(ptoggleJoue){
-    ptoggleJoue.addEventListener("click",text_supr2);
-}
-function text_supr2(){
-    ptoggleJoue.classList.remove('p__toggle');
-    iconeUser2.classList.remove('blurr__icone');
-}
-
-/* arti */
-var ptoggleArti = document.querySelector(".p__arti");
-var iconeUser3 =  document.querySelector(".icone__user3");
-if(iconeUser3){
-    iconeUser3.addEventListener("click",text_add3);
-}
-function text_add3(){
-    ptoggleArti.classList.add('p__toggle');
-    iconeUser3.classList.add('blurr__icone');
-}
-if(ptoggleArti){
-    ptoggleArti.addEventListener("click",text_supr3);
-}
-function text_supr3(){
-    ptoggleArti.classList.remove('p__toggle');
-    iconeUser3.classList.remove('blurr__icone');
-}
-
 window.addEventListener('scroll', function() {
     var menuLinks = document.querySelectorAll('.nav__link');
   
@@ -74,49 +20,27 @@ window.addEventListener('scroll', function() {
 
  /* user btn */
 var cadreUser = document.querySelector(".legende__user");
+var ptoggleUser = document.querySelector(".legende__user--grid");
 if(cadreUser){
     cadreUser.addEventListener("click", afficherCadre);
 }
 
 function afficherCadre() {
-    document.getElementById('cadre-user').style.display = 'block';
-    document.getElementById('cadre-user').style.animation = 'fade-in 0.3s forwards';
+    ptoggleUser.classList.toggle('p__toggle');
+
+    // Récupérer le pseudo-élément ::before
+    var pseudoElementBefore = window.getComputedStyle(cadreUser, '::before');
+
+    // Vérifier le contenu actuel et le changer
+    if (pseudoElementBefore.content === '"→ "') {
+        cadreUser.style.setProperty('--arrow-content', '"↓ "');
+    } else {
+        cadreUser.style.setProperty('--arrow-content', '"→ "');
+    }
+
 
 }
 
-var croixUser =  document.getElementById('croix-user');
-if(croixUser){
-    croixUser.addEventListener('click', masquerCadre);
-}
-
-function masquerCadre() {
-    document.getElementById('cadre-user').style.display = 'none';
-    document.getElementById('cadre-user').style.animation = 'fade-out 0.3s forwards';
-
-  }
-/* renc btn */
-var cadreRenc = document.querySelector(".legende__renc");
-if(cadreRenc){
-    cadreRenc.addEventListener("click", afficherCadreR);
-}
-
-  
-function afficherCadreR() {
-    document.getElementById('cadre-renc').style.display = 'block';
-    document.getElementById('cadre-renc').style.animation = 'fade-in 0.3s forwards';
-
-}
-
-var croixRenc =  document.getElementById('croix-renc');
-if(croixRenc){
-    croixRenc.addEventListener('click', masquerCadreR);
-}
-
-function masquerCadreR() {
-    document.getElementById('cadre-renc').style.display = 'none';
-    document.getElementById('cadre-renc').style.animation = 'fade-out 0.3s forwards';
-
-  }
 
 
 var blurrHypo1 = document.querySelector('.hypo__para--1')
